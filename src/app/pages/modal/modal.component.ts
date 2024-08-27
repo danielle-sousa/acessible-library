47d67151fd051716353e99710612dd89aa651e6b
danielle-sousa <daniellexsousa@gmail.com>
2024-08-27 11:11:18 -0300

    hide scroll

--- src/app/pages/modal/modal.component.ts
+++ src/app/pages/modal/modal.component.ts
@@ -1,9 +1,11 @@
 import {
   Component,
+  ElementRef,
   EventEmitter,
   HostListener,
   Input,
   Output,
+  Renderer2 
 } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { A11yModule } from '@angular/cdk/a11y';
@@ -22,7 +24,10 @@ export class ModalComponent {
   statusModal: boolean = true;
   @Output() mudouModal = new EventEmitter<boolean>()
 
-  constructor() {}
+  constructor(
+    private renderer: Renderer2,
+    private element: ElementRef
+  ) {}
 
   @HostListener('document:keydown.escape') fecharModalEsc() {
     if(this.statusModal){
@@ -33,6 +38,8 @@ export class ModalComponent {
   fecharModal() {
     this.statusModal = false
     this.mudouModal.emit(this.statusModal)
+    this.renderer.setStyle(
+      this.element.nativeElement.ownerDocument.body, 'overflow', 'scroll')
   }
 
   lerPrevia() {
